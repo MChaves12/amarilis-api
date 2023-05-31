1d51a7275ed384f285241e0bf21357339acb6a3c
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-05-31 10:45:51 -0300

    database connection created and initialized

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 //DataBase
-
+require('./db');
 
 //Configs
 
---
+++ db/index.js
@@ -0,0 +1,21 @@
+const mongoose = requie('mongoose');
+
+const DB_URI = process.env.MONGO_URI;
+
+const connect = async () => {
+    console.log('Connecting with database...');
+    try {
+        if(!DB_URI) {
+            throw new Error('No database found!');
+        }
+
+        const x = await mongoose.connect(DB_URI);
+        const databaseName = x.connections[0].name;
+        console.log(`Database coneccted at: ${databaseName}`);
+    } catch (error) {
+        console.log('Fail to connect in to the database:', error);
+        process.exit();        
+    }
+};
+
+connect();
