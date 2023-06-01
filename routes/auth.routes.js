842f5ec6f185e48e64fa8d394c0a5e2c2994f24e
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-06-01 12:16:37 -0300

    Signup route created and all the verifictations made

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,10 +2,46 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
-const User = require('../models/User.model');
+//const User = require('../models/User.model');
 
 router.get('/', (req, res, next) => {
     res.json('All good!')
+});
+
+router.post('/signup', (req, res, next) => {
+    const { username, email, password } = req.body;
+    try {
+        if(!username || !email || !password) {
+            res.status(400).json({message: 'Fill in the required fields'});
+            return;
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+        if(!emailRegex.test(email)) {
+            res.status(400).json({message: 'Insert a valid email'});
+            return;
+        }
+
+        const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+        if(!passwordRegex.test(password)) {
+            res.status(400).json({message: 'The password must have 6 caracters and at least 1 lowercase letter, 1 uppercase letter and 1 number'});
+            return;
+        }
+
+        const userEmail = await User.findOne({ email });
+        if(userEmail) {
+            res.status(400).json({message: 'Email already registerd. Insert a new one'});
+            return;
+        }
+
+        const passwordHash = bcrypt.hashSync(password, 10);
+        
+        const userFromDB = await User.create({ username, email, password: passwordHash });
+        res.status(201).json({message: `User created`});
+
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
