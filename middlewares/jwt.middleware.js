c68c6c66b48636b39913b493b9397a50fc5d3788
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-06-02 16:20:04 -0300

    authentication middleware created

---
+++ middlewares/jwt.middleware.js
@@ -0,0 +1,22 @@
+const { expressjwt: jwt } = require('express-jwt');
+
+const headersToken = (req) => {
+    const { authorization } = req.headers;
+    if (!authorization) return null;
+
+    const [type, token] = authorization.split(' ');
+    if (type === 'Bearer') return token;
+
+    return null;
+}
+
+const authenticated = jwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256'],
+    requestProperty: 'payload',
+    getToken: headersToken,
+});
+
+module.exports = {
+    authenticated,
+};
