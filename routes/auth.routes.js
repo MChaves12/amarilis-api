8d2f41d967b8af8adc943c2f1f3d401b0e052602
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-06-02 16:26:31 -0300

    authentication middleware imported to de auth routes file

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
+const { authenticated } = require('../middlewares/jwt.middleware');
+
 const User = require('../models/User.model');
 
 router.get('/', (req, res, next) => {
@@ -79,7 +81,7 @@ router.post('/login', async (req, res, next) => {
     }
 });
 
-router.get('verify', (req, res) => {
+router.get('verify', authenticated, (req, res) => {
     res.json(req.payload);
 })
 
