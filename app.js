c78271bd112573ac043f8b156106bfccfb84c1d3
Matheus Chaves <matheusca@hotmail.com>
2023-06-03 12:51:11 -0300

    Get User created

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ require('./configs')(app);
 //Routes
 app.use('/auth', require('./routes/auth.routes'));
 
+app.use('/user', require('./routes/user.routes'));
+
 //Erros
 app.use((req, res, next) => {
     res.status(404).json('Not Found!');
